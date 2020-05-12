5b7eb28238fb3bf619d88359af4be1f493f33dfc
pratikmistry-99 <pmist017@uottawa.ca>
2020-05-12 19:26:01 -0400

    added JS animation

---
+++ app.js
@@ -0,0 +1,22 @@
+const titles = ['Full-Stack Developer','IT Security Analyst'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type(){
+	setTimeout(1000);
+	if(count == titles.length){
+		count = 0;
+	}
+	currentText = titles[count];
+	letter = currentText.slice(0,++index);
+
+	document.querySelector('.user-box .typing').textContent = letter;
+	if(letter.length === currentText.length){
+		count++;
+		index = 0;
+	}
+	setTimeout(type, 200);
+
+}());
