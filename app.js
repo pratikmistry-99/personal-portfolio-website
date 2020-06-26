0e22ec167f9924c919c1caba205e2cf6bd360f42
pratikmistry-99 <pmist017@uottawa.ca>
2020-06-25 20:04:18 -0400

    fixes typewriter animation

--- app.js
+++ app.js
@@ -1,23 +1,58 @@
-const titles = ['Full-Stack Developer','IT Security Analyst'];
-let count = 0;
-let index = 0;
-let currentText = '';
-let letter = '';
+const TypeWriter = function(txtElement, words, wait = 2000){
+	this.txtElement = txtElement;
+	this.words = words;
+	this.txt = '';
+	this.index = 0;
+	this.wait = parseInt(wait, 10);
+	this.type();
+	this.isDeleting = false;
+}
 
-(function type(){
+TypeWriter.prototype.type = function(){
+	//current index
+	const i = this.index % this.words.length;
+	// get full text of current word
+	const fulltxt = this.words[i];
 
-	if(count == titles.length){
-		count = 0;
+	//check if deleting
+	if(this.isDeleting){
+		//remove char
+		this.txt = fulltxt.substring(0, this.txt.length - 1);
 	}
-	currentText = titles[count];
-	letter = currentText.slice(0,++index);
-
-	document.querySelector('.user-box .typing').textContent = letter;
-	if(letter.length === currentText.length){
-		count++;
-		index = 0;
+	else{
+		//add char
+		this.txt = fulltxt.substring(0, this.txt.length + 1);
 	}
-	setTimeout(type, 200);
+
+	this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+	
+	let typeSpeed = 100;
+	if(this.isDeleting){
+		typeSpeed /= 2;
+	}
+
+	if(!this.isDeleting && this.txt === fulltxt){
+		typeSpeed = this.wait;
+		
+		this.isDeleting = true;
+	}
+	else if(this.isDeleting && this.txt === '') {
+		this.isDeleting = false;
+
+		this.index++;
+		typeSpeed = 200;
+	}
 	
 
-}());
+	setTimeout(() => this.type(), typeSpeed);
+}
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init(){
+	const txtElement = document.querySelector('.txt-type');
+	const words = JSON.parse(txtElement.getAttribute('data-words'));
+	const wait = txtElement.getAttribute('data-wait');
+
+	new TypeWriter(txtElement, words, wait);
+}
